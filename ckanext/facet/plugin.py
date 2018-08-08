ea085bf4a344d4b02ea90f0b3ceb96390df3a841
Vic Ding <qiqing.ding@meertens.knaw.nl>
2018-08-08 18:01:25 +0200

    add template helpers: facet_loadjson and facet_apisearch

--- ckanext/facet/plugin.py
+++ ckanext/facet/plugin.py
@@ -1,14 +1,43 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import logging
+import json
+import urllib2
 
 log = logging.getLogger(__name__)
+
+def facet_loadjson(orgstr, swap=True):
+    '''return json obj'''
+    json_data = json.loads(orgstr)
+    if swap:
+        if json_data['type'] == 'Point':
+            json_data['coordinates'] = [[json_data['coordinates'][1], json_data['coordinates'][0]]]
+        else:
+            json_data['coordinates'] = map(lambda x: list(reversed(x)), reversed(json_data['coordinates']))
+    return json_data
+
+def facet_apisearch(q='', rows=999999):
+    request = urllib2.Request(
+        'http://ckan:5000/api/3/action/package_search?q=%s&rows=%s' % (q, rows))
+
+    # Creating a dataset requires an authorization header.
+    # request.add_header('Authorization', apikey)
+
+    # Make the HTTP request.
+    response = urllib2.urlopen(request)
+    assert response.code == 200
+
+    # Use the json module to load CKAN's response into a dictionary.
+    response_dict = json.loads(response.read())
+    assert response_dict['success'] is True
+    return response_dict['result']
 
 
 class FacetPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IFacets)
     plugins.implements(plugins.IPackageController, inherit=True)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def dataset_facets(self, facets_dict, package_type):
         return self._facets(facets_dict)
@@ -50,3 +79,13 @@ class FacetPlugin(plugins.SingletonPlugin):
         # 'templates' is the path to the templates dir, relative to this
         # plugin.py file.
         toolkit.add_template_directory(config, 'templates')
+
+    def get_helpers(self):
+        '''Register the most_popular_groups() function above as a template
+        helper function.
+
+        '''
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'facet_loadjson': facet_loadjson, 'facet_apisearch': facet_apisearch}
